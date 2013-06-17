2b3b3a5410da3e2381aeb3e02bf409672acff221
Jimmy Do <jimmydo@users.noreply.github.com>
2013-06-16 23:42:38 -0700

    Add package URL

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from distutils.core import setup
 
 setup(
     name='grain',
-    version='0.1.3',
+    version='0.1.4',
     description='Simple, extensible test assertions.',
     long_description=open('README.md').read(),
     author='Jimmy Do',
     license='MIT',
-    py_modules=['grain'])
+    py_modules=['grain'],
+    url='https://github.com/jimmydo/grain')
